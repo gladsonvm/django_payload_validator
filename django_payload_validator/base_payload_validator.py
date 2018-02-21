deaf06de311fce3fcded706eb1c90431b45487a1
Gladson V Manuel <gladsonvm@gmail.com>
2018-02-21 16:28:21 +0530

    code made compatible with python2.x

--- django_payload_validator/base_payload_validator.py
+++ django_payload_validator/base_payload_validator.py
@@ -1,6 +1,7 @@
 from response.response import Response
 from django.http import JsonResponse
 from operator import attrgetter
+import json
 
 
 class BasePayloadValidator(object):
@@ -11,7 +12,7 @@ class BasePayloadValidator(object):
 
     def validate_request_data(self):
         """
-        This method is the entry point for provide formatted data after successful validation.
+        This method is the entry point for BasePayloadValidator and provided formatted data after successful validation.
         :return: None if validation passes if any one validation fails, then appropriate error msg.
         """
         validators = [self.validate_request_body, self.validate_mandatory_params, self.validate_allowed_params]
@@ -30,7 +31,6 @@ class BasePayloadValidator(object):
                 return False
         return True
 
-
     def validate_mandatory_params(self):
         """
         This method checks if all mandatory params are there in a given json.
@@ -38,11 +38,10 @@ class BasePayloadValidator(object):
         """
         mandatory_params = self.get_mandatory_param()
         if mandatory_params:
-            if len([x for x in [*mandatory_params] if x not in [*self.data]]):
+            if len([x for x in mandatory_params if x not in list(self.data)]):
                 return False, {'error': 'mandatory params missing. mandatory parameters are {mandatory_params}'
-                    .format(mandatory_params=mandatory_params)}
+                                        .format(mandatory_params=mandatory_params)}
         return True,
-
 
     def validate_request_body(self):
         """
@@ -55,14 +54,13 @@ class BasePayloadValidator(object):
             return False, {'error': 'provide a valid json.'}
         return True,
 
-
     def validate_allowed_params(self):
         """
         check if any invalid params are in request body.
         :return: False and error msg if validation fails else True.
         """
         allowed_params = self.get_all_params()
-        sorted_request_params = sorted([*self.data])
+        sorted_request_params = sorted(list(self.data))
         sorted_allowed_params = sorted(allowed_params)
         invalid_params = [x for x in sorted_request_params if x not in sorted_allowed_params]
         if invalid_params:
@@ -70,14 +68,12 @@ class BasePayloadValidator(object):
                 .format(invalid_params=invalid_params)}
         return True,
 
-
     def get_validation_rule(self):
         """
         return validation rule specified in inherited class.
         :return: self.validation rule, a json.
         """
         return self.validation_rule
-
 
     def is_valid(self):
         """
@@ -87,7 +83,6 @@ class BasePayloadValidator(object):
         validation_response = self.validate_request_data()
         return validation_response
 
-
     def json_valid(self):
         """
         triggered if a json is validated successfully against a given rule.
@@ -96,7 +91,6 @@ class BasePayloadValidator(object):
         self.object = self.save()
         return JsonResponse(self.render_json(self.object, status_code=201))
 
-
     def json_invalid(self):
         """
         invoked if a json is not validated successfully against a given rule.
@@ -104,26 +98,23 @@ class BasePayloadValidator(object):
         """
         return JsonResponse(self.error_dict, status=400)
 
-
     def get_mandatory_param(self):
         """
         Return mandatory parameters in validation rule.
         :return: all mandatory params in validation rule
         if rule have mandatory params else None.
         """
-        mandatory_params = [k for k, v in self.validation_rule.items()
+        mandatory_params = [k for k, v in self.validation_rule.get('fields').items()
                             if self.validation_rule.get(k).get('required') is not None]
 
         return mandatory_params if mandatory_params else None
-
 
     def get_all_params(self):
         """
         Returns all params given in a validation rule.
         :return: all params in a given validation rule
         """
-        return [*self.validation_rule['fields']]
-
+        return list(self.validation_rule['fields'])
 
     def save(self, data=None):
         """
@@ -137,7 +128,6 @@ class BasePayloadValidator(object):
             data.update({k: attrgetter(v)(self) for k, v in self.auto_populate_fields.items()})
         self.instance = self.model.objects.create(**data)
         return self.instance
-
 
     def render_json(self, obj, status_code):
         """
